1617f3aa3971c066a8a637dbd329740c5a99f12e
Olli-Pekka Aro <olli-pekka@identio.fi>
2024-08-06 09:46:11 +0300

    add new test, finish 11.9.

--- test/e2e/frontpage.spec.js
+++ test/e2e/frontpage.spec.js
@@ -6,5 +6,13 @@ describe('Pokedex', () => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  }),
+
+  test('a single pokemon page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+    await page.getByRole('link', { name: 'ivysaur' }).click();
+    await expect(page.getByText('chlorophyll')).toBeVisible();
   })
 })
